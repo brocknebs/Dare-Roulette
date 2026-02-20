94dec2fa92c17b0098586a127bbe88c539834eff
brocknebs <brockneb@gmail.com>
2026-02-20 14:07:01 -0700

    Add dynamic port for Render

--- server.js
+++ server.js
@@ -176,6 +176,8 @@ function handleEvent(code, player, event) {
   io.to(code).emit("start_minigame", { type: event, player });
 }
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
   console.log("Server running on http://localhost:3000");
 });
